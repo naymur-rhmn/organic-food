05d9aff651ebdf917856a69922fad60af95c9ec2
Naymur Rahman <naymurmn@gmail.com>
2024-06-06 10:21:16 +0600

    environment-variable

--- src/authentication/firebase.config.js
+++ src/authentication/firebase.config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAaPCmlVKVgj0m49cTykpdqc2Rc6mlIy-4",
-  authDomain: "organic-food-3a04d.firebaseapp.com",
-  projectId: "organic-food-3a04d",
-  storageBucket: "organic-food-3a04d.appspot.com",
-  messagingSenderId: "359767198872",
-  appId: "1:359767198872:web:dc5e8cbb01e3128bb914ed",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
